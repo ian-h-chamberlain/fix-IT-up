370d1bf0577f6d5093e266479b41f53254e9584f
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2024-06-22 21:57:30 -0400

    Fix up some timer weirdness in the buttons

--- scripts/Desktop.gd
+++ scripts/Desktop.gd
@@ -2,60 +2,66 @@ extends Node
 
 enum Power {ON, OFF}
 
-@export var power_state: Power = Power.OFF
-@export var floppy_inserted: bool = true
-@export var cd_inserted: bool = true
+@export var power_state: Power = Power.ON
+
+@export var floppy_inserted: bool = true:
+	set(inserted):
+		floppy_inserted = inserted
+		_toggle_blinking($Floppy/LED, inserted)
+
+@export var cd_inserted: bool = true:
+	set(inserted):
+		cd_inserted = inserted
+		_toggle_blinking($CD/LED, inserted)
 
 func _ready():
 	$PowerButton.pressed.connect(_on_power_pressed)
-	$HardDisk/LED/Timer.timeout.connect(func(): _toggle_vis($HardDisk/LED))
+	$HardDisk/LED/Timer.timeout.connect(func(): _blink_and_reset_timer($HardDisk/LED))
 
-	$Floppy/EjectButton.pressed.connect(func(): _toggle_vis($Floppy/LED))
-	$Floppy/LED/Timer.timeout.connect(func(): _toggle_vis($Floppy/LED))
+	$Floppy/EjectButton.pressed.connect(func(): floppy_inserted=not floppy_inserted)
+	$Floppy/LED/Timer.timeout.connect(func(): _blink_and_reset_timer($Floppy/LED))
 
-	$CD/EjectButton.pressed.connect(func(): _toggle_vis($CD/LED))
-	$CD/LED/Timer.timeout.connect(func(): _toggle_vis($CD/LED))
+	$CD/EjectButton.pressed.connect(func(): cd_inserted=not cd_inserted)
+	$CD/LED/Timer.timeout.connect(func(): _blink_and_reset_timer($CD/LED))
 
 	if power_state == Power.ON:
-		_toggle_vis($HardDisk/LED)
+		$PowerLED.visible = true
+		_blink_and_reset_timer($HardDisk/LED)
+
 		if floppy_inserted:
-			_toggle_vis($Floppy/LED)
+			_blink_and_reset_timer($Floppy/LED)
 
 		if cd_inserted:
-			_toggle_vis($CD/LED)
+			_blink_and_reset_timer($CD/LED)
 
-func _toggle_vis(led: ColorRect):
+func _toggle_blinking(led: ColorRect, inserted: bool, delay: float=- 1.0):
+	var timer := led.get_node("Timer") as Timer
+
+	if not inserted:
+		led.visible = false
+		timer.stop()
+	else:
+		led.visible = true
+		_blink_and_reset_timer(led, delay)
+
+func _blink_and_reset_timer(led: ColorRect, wait_time: float=- 1.0):
 	led.visible = not led.visible
 	var timer := led.get_node("Timer") as Timer
 	timer.wait_time = randf_range(0.05, 0.6)
-	timer.start()
+	timer.start(wait_time)
 
 func _on_power_pressed():
 	match power_state:
-		Power.OFF: power_state = Power.ON
-		Power.ON: power_state = Power.OFF
+		Power.OFF:
+			power_state = Power.ON
+			$PowerLED.visible = true
+			_toggle_blinking($HardDisk/LED, true, randf_range(0.5, 1.5))
+			_toggle_blinking($CD/LED, cd_inserted, randf_range(1.5, 3.0))
+			_toggle_blinking($Floppy/LED, floppy_inserted, randf_range(1.5, 2.5))
 
-	_toggle_leds()
-
-func _toggle_leds():
-	for timer in [
-		$CD/LED/Timer,
-		$Floppy/LED/Timer,
-		$HardDisk/LED/Timer,
-	]:
-		if timer.is_stopped() and power_state == Power.ON:
-			timer.wait_time += randf_range(1.0, 3.0)
-			timer.start()
-		elif not timer.is_stopped() and power_state == Power.OFF:
-			timer.stop()
-
-	if power_state == Power.OFF:
-		for led in [
-			$PowerLED,
-			$CD/LED,
-			$Floppy/LED,
-			$HardDisk/LED,
-		]:
-			led.visible = false
-	else:
-		$PowerLED.visible = true
+		Power.ON:
+			power_state = Power.OFF
+			$PowerLED.visible = false
+			_toggle_blinking($HardDisk/LED, false)
+			_toggle_blinking($CD/LED, false)
+			_toggle_blinking($Floppy/LED, false)
