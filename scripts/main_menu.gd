27f11a428632148557a4faddc10336f88727929e
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2024-06-23 22:19:08 -0400

    Add desktop icons, fix web resizing problem
    Also minor theme fix to get proper font in popup dialog

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -10,12 +10,13 @@ func _ready():
     $Menu/Buttons/Settings.pressed.connect(func(): $SettingsDialog.visible=true)
     $Menu/Buttons/Help.pressed.connect(func(): $HelpDialog.visible=true)
 
+    if OS.has_feature("web"):
+        $Menu/Buttons/Exit.visible = false
+    else:
+        $Menu/Buttons/Exit.pressed.connect(func(): get_tree().quit())
+
     $Menu/Credits.meta_clicked.connect(_credits_on_meta_clicked)
 
 # https://docs.godotengine.org/en/4.2/tutorials/ui/bbcode_in_richtextlabel.html#handling-url-tag-clicks
 func _credits_on_meta_clicked(meta):
     OS.shell_open(str(meta))
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    pass
